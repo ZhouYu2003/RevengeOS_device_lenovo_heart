ce8e52d7bcd9e820ba68689a23b8ed814e125e42
ZhouYu2003 <2359292349@qq.com>
2021-01-28 20:35:28 +0800

    heart: Delete camera blobs

--- device.mk
+++ device.mk
@@ -118,14 +118,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
-
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
-    libdng_sdk \
-    libgui_vendor \
-    libqti-perfd-client \
-    libxml2 \
+    libdng_sdk.vendor \
     Snap
 
 PRODUCT_COPY_FILES += \
